a52a82eb6d6503ce99cd1f6c6011421d811a85d0
foucauld <foucauldbeaumont@yahoo.fr>
2022-08-30 22:40:07 +0200

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show update destroy ]
+  before_action :authenticate_user!, only: %i[ new create edit udpate destroy ]
+
+  # GET /articles
+  def index
+    @articles = Article.all
+    render json: @articles.as_json(include: {user: {only: :email}})
+    # render json: @comments.as_json(only: :content, include: [{user: {only: :email}}, {article: {only: :title}}] )
+  end
+
+  # GET /articles/1
+  def show
+    render json: @article.as_json(include: :user)
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+
+    if @article.save
+      render json: @article, status: :created, location: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    if @article.user_id === current_user.id && @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @article.destroy
+    if (@article.user_id === current_user.id)
+      @article.destroy
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+end
